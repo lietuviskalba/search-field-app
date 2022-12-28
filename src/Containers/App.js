529c8f162acdcb2db494ebe20f4072bcb28a8f5e
Mantas Lingaitis <mantasling@gmail.com>
2022-12-28 22:12:29 +0200

    > File and folder refactor

---
+++ src/Containers/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import './App.css';
+import SearchBox from "../Components/SearchBox";
+import Scroll from '../Components/Scroll.js';
+import CardList from '../Components/CardList.js';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            users: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        // fetch make request from servers (part of the window object)
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(usersFromJSON => this.setState({ users: usersFromJSON }))
+    }
+
+    // Used for the search box to detect and give changes
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+    }
+
+    render() {
+        const { users, searchField } = this.state;
+        const filteredUsers = users.filter(user => {
+            return user.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        return !users.length ?
+            <h1 className="tc"> Loading users </h1>
+            :
+            <div className="tc">
+                <h1> User search app</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <CardList users={filteredUsers} />
+                </Scroll>
+            </div>
+
+    }
+}
+
+
+export default App;

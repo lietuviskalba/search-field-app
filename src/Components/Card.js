201eb121dfdff192a9fd6e6c838e297df3dbb57d
Mantas Lingaitis <mantasling@gmail.com>
2022-12-29 11:51:43 +0200

    > User card name color update

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,7 +7,7 @@ const Card = ({ id=-1, name="Default name", email="default@email.fix" }) => {
     <div className="bg-dark-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt="userCard" src={`https://robohash.org/}${id}?set=set5`} />
       <div>
-        <h2> {name}</h2>
+        <h2 className="color red"> {name}</h2>
         <h3> {email}</h3>
       </div>
     </div>
